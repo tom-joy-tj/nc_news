16b208d231a4526cda15eec53deb714755124fae
tom joy <thomaspjoy1@gmail.com>
2025-04-28 13:56:36 +0100

    implemented kev's suggested edits to the listener

--- listen.js
+++ listen.js
@@ -1,16 +1,7 @@
 const app = require("./app.js");
 
-const dotenv = require("dotenv");
-
-
-if (process.env.NODE_ENV !== 'development') {
-    dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
-  }
 const { PORT = 2025 } = process.env;
 
 app.listen(PORT, () => {
     consaole.log('Server is listening on PORT ${PORT}...')
 })
-
-// I know I wont be using this listener whilst running supertest 
-// I am unsure if something is missing here for this listener to work as intended 
