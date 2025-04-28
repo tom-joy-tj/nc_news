81a66d49d4289a7e85d05fe410109d39371ed32c
tom joy <thomaspjoy1@gmail.com>
2025-04-28 20:33:12 +0100

    updated app to handle errors and built file for error handling controllers

--- app.js
+++ app.js
@@ -3,14 +3,27 @@ const app = express();
 
 const { getAPI, getTopics, getArticlesByID } = require("./controllers/controller.js");
 
+const { handlePsqlError, handleCustomError, handle500Error } = require("./controllers/error.controller.js");
+
+app.use(express.json());
+
 app.get("/api", getAPI);
 
 app.get("/api/topics", getTopics);
 
 app.get("/api/articles/:article_id", getArticlesByID)
 
+
+
+
 app.all("/*splat", (req, res) => {
-    res.status(404).send({ msg: "Endpoint not found" });
+    res.status(404).send({ msg: "Endpoint not found!" });
   });
+
+app.use(handlePsqlError);
+
+app.use(handleCustomError);
+
+app.use(handle500Error);
 
 module.exports = app;
---
+++ controllers/error.controller.js
@@ -0,0 +1,26 @@
+exports.handlePsqlError = (err, req, res, next) => {
+
+    if (err.code === "22P02") {
+        res.status(400).send( { msg: "BAD REQUEST - PSQL ERROR!" } )
+    } 
+    else {
+        next(err) 
+    }
+};
+
+exports.handleCustomError = (err, req, res, next) => {
+    
+    if (err.status && err.msg) {
+        res.status(err.status).send({ msg : err.msg })
+    }
+    else { 
+        next(err) 
+    }
+};
+
+exports.handle500Error = (err, req, res, next) => {
+
+    console.log(err, "<<<<<<<<<<< ERROR HAS REACHED THE UNKNOWN ERROR HANDLER - THIS MUST BE AN UNHANDLED ERROR")
+
+    res.status(500).send( { msg: "Internal server error!" } )
+};
