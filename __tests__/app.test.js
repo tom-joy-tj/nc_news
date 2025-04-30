816f14b52d8773f6dd9f03eb81abb1de211f5480
tom joy <thomaspjoy1@gmail.com>
2025-04-30 12:25:47 +0100

    task 7 resubmit - added 400 test for inc_votes not a number

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -288,6 +288,16 @@ describe("PATCH /api/articles/:article_id", () => {
       expect(body.msg).toBe("BAD REQUEST - PSQL ERROR!") //PSQL error from global err handler 
     })
   }); 
+
+  test.only("400: responds with psql error message when article_id is valid but inc_votes is NOT A NUMBER", () => {
+    return request(app)
+    .patch("/api/articles/1") //valid article_id
+    .send( { inc_votes: "Not_a_Number" } ) // NOT A NUMBER
+    .expect(400)
+    .then(( { body } ) => {
+      expect(body.msg).toBe("BAD REQUEST - PSQL ERROR!") //PSQL error from global err handler 
+    })
+  }); 
 }); 
 
 describe("DELETE /api/comments/:comment_id", () => {
@@ -319,7 +329,7 @@ describe("DELETE /api/comments/:comment_id", () => {
   });
 });
 
-describe.only("GET /api/users", () => {
+describe("GET /api/users", () => {
   test("200: Returns an array of user objects. Each user object should contain username, name, avatar_url properties", () => {
     return request(app)
     .get("/api/users")
