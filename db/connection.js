b506d48d6edc48ed6fa9b8d35b3de76ee8dd811e
tom joy <thomaspjoy1@gmail.com>
2025-05-01 14:05:59 +0100

    hosting fixes

--- db/connection.js
+++ db/connection.js
@@ -1,30 +1,23 @@
-const ENV = process.env.NODE_ENV || 'development'
-
-require('dotenv').config({path: `${__dirname}/../.env.${ENV}`})
-
 const { Pool } = require("pg");
 
+const ENV = process.env.NODE_ENV || 'development';
 
-
-const db = new Pool();
-
-const config = {};
-
+require('dotenv').config({path: `${__dirname}/../.env.${ENV}`})
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
     throw new Error("PGDATABASE or DATABASE_URL not set");
   }
 else { 
-    console.log(`Connected to ${process.env.PGDATABASE}`)
+    console.log(`Connected to ${process.env.PGDATABASE || process.env.DATABASE_URL}`)
 }
+
+const config = {};
 
 if (ENV === "production") {
     config.connectionString = process.env.DATABASE_URL;
     config.max = 2;
 }
 
-  module.exports = new Pool(config);
+const db = new Pool(config);
 
 module.exports = db;
-
-
