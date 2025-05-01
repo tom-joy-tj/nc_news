a78ca609b45df5ddadca5d0936b40609cc11972b
tom joy <thomaspjoy1@gmail.com>
2025-05-01 13:12:22 +0100

    setting up prod env

--- db/connection.js
+++ db/connection.js
@@ -1,16 +1,30 @@
-const { Pool } = require("pg");
-
 const ENV = process.env.NODE_ENV || 'development'
 
 require('dotenv').config({path: `${__dirname}/../.env.${ENV}`})
 
+const { Pool } = require("pg");
+
+
+
 const db = new Pool();
 
-if (!process.env.PGDATABASE) {
-    throw new Error("No PGDATABASE configured")
-} else { 
+const config = {};
+
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+    throw new Error("PGDATABASE or DATABASE_URL not set");
+  }
+else { 
     console.log(`Connected to ${process.env.PGDATABASE}`)
 }
 
+if (ENV === "production") {
+    config.connectionString = process.env.DATABASE_URL;
+    config.max = 2;
+}
+
+  module.exports = new Pool(config);
 
 module.exports = db;
+
+
--- listen.js
+++ listen.js
@@ -1,6 +1,6 @@
 const app = require("./app.js");
 
-const { PORT = 2025 } = process.env;
+const { PORT = 9090 } = process.env;
 
 app.listen(PORT, () => {
     console.log('Server is listening on PORT ${PORT}...')
